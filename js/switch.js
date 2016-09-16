918be6d8afd1211c73353e15cf0c7962c86ba951
Alexander Andreason <10529489@AD.UVU.EDU>
2016-09-16 11:36:11 -0600

    adding stuff

--- js/switch.js
+++ js/switch.js
@@ -3,12 +3,38 @@
 var petName = prompt("what is my pets name?", "Type name here.");
 
 switch (petName) {
-    case "Rover";
+    case "Rover":
         alert("My dog is named " + petName);
         break;
     
-    default:
-        alert("That is not my pets name.")
+    case "Lassy":
+        alert("My friends dog is named " + petName);
         break;
 
+    case "Captain lipstick":
+        alert("A dog named " + petName + " bit my toes off");
+    
+    default:
+        alert("That is not my pets name.")
+        break;1
+}
+
+var gameLevel = 1;
+var password = "mellon";
+
+switch (gameLevel) {
+    case 1:
+        prompt("Speak friend and enter.");    
+        break;
+
+    case 2:
+    
+        break;
+    
+    case 3:
+    
+        break;
+    
+    default:
+        break;
 }
