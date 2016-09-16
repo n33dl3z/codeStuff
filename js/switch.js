5a2e8e35ebe279c87c1603428fa1f93c1aa584d3
Alexander Andreason <10529489@AD.UVU.EDU>
2016-09-16 11:35:26 -0600

    doin stuff with switches

---
+++ js/switch.js
@@ -0,0 +1,14 @@
+//string var
+
+var petName = prompt("what is my pets name?", "Type name here.");
+
+switch (petName) {
+    case "Rover";
+        alert("My dog is named " + petName);
+        break;
+    
+    default:
+        alert("That is not my pets name.")
+        break;
+
+}
